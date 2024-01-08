b73538a2fbbf9716299ae9abbc902b4bf809fe5f
Towhid Hossain <zisankhanchowdhury@gmail.com>
2024-01-08 18:10:39 +0600

    removed comments

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', async (req, res) => {
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j1gum5d.mongodb.net/?retryWrites=true&w=majority`;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+   
     await client.connect();
 
     const database = client.db('brandBD')
@@ -101,7 +101,7 @@ async function run() {
       res.send(result)
     })
 
-    // Send a ping to confirm a successful connection
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
